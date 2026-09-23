c4bc8202a9a143dbc67f9b00eeb0c0d188493f99
Komal Priya <kryptonbenzene@gmail.com>
2026-09-23 18:02:11 +0530

    Add cookie-parser dependency and setup database connection

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express ();
+const connectDB=require("./src/config/config.js");
+const cookieParser = require("cookie-parser");
+connectDB()
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(3000,()=>{
+    console.log("Server is running at port 3000");
+});
---
+++ backend/src/config/config.js
@@ -0,0 +1,16 @@
+
+
+console.log("config.js loading")
+
+const mongoose = require('mongoose')
+const connectDB= async()=>{
+    try{
+await mongoose.connect(process.env.DATABASE_URI)
+console.log("✅" +  " MongoDB connected")
+    }
+    catch(error){
+console.log("MongoDB connetion failed")
+process.exit(1)
+    }
+}
+module.exports= connectDB
